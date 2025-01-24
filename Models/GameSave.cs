8fec0b3c6af62bf2de451cd07f5fd9e532e43705
Jônatas da Silva Gonçalves <145167220+JnSGoncalves@users.noreply.github.com>
2025-01-24 18:40:13 -0300

    ajuste janela, add de assets e primeiras classes

---
+++ Models/GameSave.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkpoint_Manager.Models {
+    internal class GameSave {
+        public String name { get; set; }
+        public String path { get; set; }
+
+        public GameSave(String name, String path) { 
+            this.name = name;
+            this.path = path;
+        }
+    }
+}
