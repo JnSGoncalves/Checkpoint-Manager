1d77184558eceb66cef0a4ec9b8a4fba7b3518c1
Jônatas da Silva Gonçalves <145167220+JnSGoncalves@users.noreply.github.com>
2025-01-25 20:04:28 -0300

    atualizações topMenu

---
+++ Models/Save.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkpoint_Manager.Models {
+    internal class Save {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public Boolean IsFavorite { get; set; }
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Checkpoint_Manager.Models;
 
 namespace Checkpoint_Manager.ViewModels {
     internal class MainViewModel {
+        public List<Game> GameSaves { get; set; }
+        
+        public void StartApp() {
+
+        }
     }
 }
