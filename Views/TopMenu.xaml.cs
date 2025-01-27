f9cbf1df1a78ad85388a490e44ad6981e1ef8d39
Jônatas da Silva Gonçalves <145167220+JnSGoncalves@users.noreply.github.com>
2025-01-27 20:19:54 -0300

    Botão list do topMenu com ação e downBar criada incialmente

--- Views/TopMenu.xaml.cs
+++ Views/TopMenu.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,11 +21,36 @@ namespace Checkpoint_Manager.Views {
     public partial class TopMenu : Page {
         public TopMenu() {
             InitializeComponent();
-            this.Loaded += TopMenu_Loaded;
         }
 
-        private void TopMenu_Loaded(object sender, RoutedEventArgs e) {
+        private void CloseSideBar(object sender, RoutedEventArgs e) {
+            var sideBarFrame = (Application.Current.MainWindow as MainWindow).FindName("sideBarFrame") as Frame;
+            if (sideBarFrame != null) {
+                var translateTransform = sideBarFrame.RenderTransform as TranslateTransform;
+                if (translateTransform != null) {
+                    // Criando a animação para mover a SideBar para fora da tela
+                    var animation = new DoubleAnimation {
+                        To = -200, // Valor negativo para mover a Sidebar para fora
+                        Duration = new Duration(TimeSpan.FromSeconds(0.3))
+                    };
+                    translateTransform.BeginAnimation(TranslateTransform.XProperty, animation);
+                }
+            }
+        }
 
+        private void OpenSideBar(object sender, RoutedEventArgs e) {
+            var sideBarFrame = (Application.Current.MainWindow as MainWindow).FindName("sideBarFrame") as Frame;
+            if (sideBarFrame != null) {
+                var translateTransform = sideBarFrame.RenderTransform as TranslateTransform;
+                if (translateTransform != null) {
+                    // Criando a animação para mover a SideBar de volta para a posição inicial
+                    var animation = new DoubleAnimation {
+                        To = 0, // Valor 0 para colocar a Sidebar de volta à posição original
+                        Duration = new Duration(TimeSpan.FromSeconds(0.3))
+                    };
+                    translateTransform.BeginAnimation(TranslateTransform.XProperty, animation);
+                }
+            }
         }
     }
 }
