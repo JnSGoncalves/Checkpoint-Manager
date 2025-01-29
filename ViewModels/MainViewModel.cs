306f1a6a3bebf4054213c1b30e93e35fb4d1f510
Jônatas da Silva Gonçalves <145167220+JnSGoncalves@users.noreply.github.com>
2025-01-28 21:19:25 -0300

    remoção de jogos no front-end criado
    remoção dos jogos da lista de jogos na mainviewmodel e na ui

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -22,11 +22,11 @@ namespace Checkpoint_Manager.ViewModels {
         }
 
         public MainViewModel() {
-            Games = new ObservableCollection<Game>();
         }
 
         public void StartApp() {
             Games = FileManeger.FindGames();
+            Console.WriteLine("1");
             Console.WriteLine(Games.Count);
             FileManeger.StartConfigInfo();
         }
@@ -34,6 +34,18 @@ namespace Checkpoint_Manager.ViewModels {
         public void AddGame(int id, String name, String path) {
             Games.Add(new Game(id, name, path));
             OnPropertyChanged(nameof(Games));
+        }
+
+        public void DelGame(int id) {
+            for (int i = 0; i < Games.Count; i++) {
+                if (Games[i].Id == id) {
+                    Games.RemoveAt(i);
+                    OnPropertyChanged(nameof(Games));
+                    break;
+                }
+            }
+
+            // Adicionar o resto do código de remoção dos arquivos
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
--- Views/SideBar.xaml.cs
+++ Views/SideBar.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Checkpoint_Manager.ViewModels;
 
 namespace Checkpoint_Manager.Views {
@@ -22,8 +12,10 @@ namespace Checkpoint_Manager.Views {
     public partial class SideBar : Page {
         public SideBar() {
             InitializeComponent();
-
             UpdateToggleButtons();
+
+            var viewModel = (MainViewModel)this.DataContext;
+            viewModel.PropertyChanged += ViewModel_PropertyChanged;
         }
 
         private void GameClick(object sender, RoutedEventArgs e) {
@@ -36,21 +28,70 @@ namespace Checkpoint_Manager.Views {
                 }
             }
 
-            if (sender is ToggleButton gameButton) {
-                String idString = gameButton.Name;
-                if (idString != null) {
-                    int index = idString.IndexOf("_");
-                    string result = idString.Substring(index + 1);
-                    int id = int.Parse(result);
-
-                    Console.WriteLine(id);
-                }
-            }
+            Console.WriteLine("Game id = " + GetSenderId(sender));
 
             var mainWindow = (Application.Current.MainWindow as MainWindow);
             if(mainWindow != null) {
                 mainWindow.teste();
             }
+        }
+
+        private void RemoveGame(object sender, RoutedEventArgs e) {
+            ToggleButton gameButton = new ToggleButton();
+            gameButton.Name = "_1";
+
+            bool noGameSelected = true;
+
+            if (list != null) {
+                foreach (var child in list.Children) {
+                    // Verifica se o item é um ToggleButton e não é o sender
+                    if (child is ToggleButton toggleButton) {
+                        if ((bool)toggleButton.IsChecked) {
+                            noGameSelected = false;
+                            gameButton = toggleButton;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (noGameSelected) {
+                MessageBox.Show("Nenhum jogo selecionado", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            int id = GetSenderId(gameButton);
+            if (id == -1) {
+                MessageBox.Show("Erro ao buscar jogo", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show("Deseja realmente remover o jogo " + gameButton.Content + 
+                "?\nEssa ação não pode ser desfeita.", "Remover Jogo", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.OK) { 
+                ((MainViewModel)this.DataContext).DelGame(id);
+
+                Console.WriteLine("Game " + id + " Removido");
+            
+                ViewDefaultContent();
+            }
+        }
+
+        // Retorna o Id do jogo relacionado ao Toggle Button
+        private int GetSenderId(object sender) {
+            if (sender is ToggleButton gameButton) {
+                String idString = gameButton.Name;
+                if (idString != null && idString != "_1") {
+                    int index = idString.IndexOf("_");
+                    string result = idString.Substring(index + 1);
+                    int id = int.Parse(result);
+
+                    return id;
+                } else
+                    return -1;
+            } else
+                return -1;
         }
 
         private void UpdateToggleButtons() {
@@ -69,13 +110,27 @@ namespace Checkpoint_Manager.Views {
             }
         }
 
-        private void ReturnToDefaultContent(object sender, RoutedEventArgs e) {
-            // Preferencialmente seria bom para a aplicação a adição de um timer
-            // para impedir de clicar muito rápido no botão
+        // Reseta a visualização do conteudo da página principal
+        private void ViewDefaultContent() {
             var mainWindow = (Application.Current.MainWindow as MainWindow);
             if (mainWindow != null) {
                 mainWindow.viewDefaultPage();
-            } 
+            }
+        }
+
+
+        private void UncheckedGame(object sender, RoutedEventArgs e) {
+            // Preferencialmente seria bom para a aplicação a adição de um timer
+            // para impedir de clicar muito rápido no botão
+            ViewDefaultContent();
+        }
+
+        // Evento criado para verificar se os itens da ViewModel foram modificados
+        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e) {
+            if (e.PropertyName == nameof(MainViewModel.Games)) {
+                // Chamar a função sempre que a propriedade modificada for a lista de Jogos
+                UpdateToggleButtons();
+            }
         }
     }
 }
