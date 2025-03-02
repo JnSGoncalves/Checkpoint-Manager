8233b10fef8d939c99775c132b70842c3464486c
Jônatas da Silva Gonçalves <145167220+JnSGoncalves@users.noreply.github.com>
2025-03-02 16:54:04 -0300

    notificação de segundo plano

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,4 +5,16 @@ public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
     }
+
+    protected override void OnStateChanged(EventArgs e) {
+        base.OnStateChanged(e);
+        if (WindowState == WindowState.Minimized) {
+            Hide();
+        }
+    }
+
+    protected override void OnClosing(System.ComponentModel.CancelEventArgs e) {
+        e.Cancel = true;
+        Hide();
+    }
 }
